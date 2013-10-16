c2e6c15f140cebdfe7d6870c1e8e459d06162081
Chris Neave <chrispneave@gmail.com>
2013-10-16 21:45:00 +0100

    Handle connection errors more gracefully

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ var httpProxy = require('http-proxy');
 var FakeMinder = require('./lib/fakeminder.js');
 
 var fm = new FakeMinder();
+var target_host = fm.config.target_site.host;
+var target_port = fm.config.target_site.port;
+var target_hostname = target_host + ':' + target_port;
 
-httpProxy.createServer(function(req, res, proxy) {
+var server = httpProxy.createServer(function(req, res, proxy) {
 
   if (fm.handleRequest(req, res)) {
+    console.log('proxying request -> ' + req.method + ' ' + req.url);
     proxy.proxyRequest(req, res, {
-      host: 'localhost',
-      port: '4567'
+      host: target_host,
+      port: target_port
     });
   }
 }).listen(8000);
+
+server.proxy.on('proxyError', function(err, req, res) {
+  if (err.code === 'ECONNREFUSED') {
+    console.log('Error -> Connection refused! Make sure the target application ' + target_hostname + ' is running');
+  }
+});
