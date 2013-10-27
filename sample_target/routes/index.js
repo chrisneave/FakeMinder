1f83ccbcceb37a015f4424d5a56b67b865b70e44
Chris Neave <chrispneave@gmail.com>
2013-10-27 13:24:36 +0000

    Added routes and views for protected and error resources

--- sample_target/routes/index.js
+++ sample_target/routes/index.js
@@ -1,8 +1,20 @@
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+exports.index = function(req, res) {
   res.render('index', { title: 'Express' });
+};
+
+exports.protected = function(req, res) {
+  res.render('protected', { title: 'Protected', headers: req.headers });
+};
+
+exports.login = function(req, res) {
+  res.render('login', { title: 'Login' });
+};
+
+exports.logoff = function(req, res) {
+  res.render('message', { title: 'Logoff', message: 'Your are now logged off. Have a nice day!'});
+};
+
+exports.not_authenticated = function(req, res) {
+  res.render('message', { title: 'Not authenticated', message: 'Your are not authenticated. Please login before accessing this resource.'});
 };
