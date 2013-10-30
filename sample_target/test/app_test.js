cfc959ec9106382a9cedd75aff430c9c44a9bad4
Chris Neave <chrispneave@gmail.com>
2013-10-30 12:58:01 +0000

    More Casper tests and additions to the homepage

--- sample_target/test/app_test.js
+++ sample_target/test/app_test.js
@@ -2,17 +2,51 @@ var fs = require('fs');
 var json = fs.read('./config.json', 'utf8');
 var fakeminder_config = JSON.parse(json);
 var base_url = fakeminder_config.target_site.root;
-var public_url = base_url + fakeminder_config.target_site.urls.public;
+var homepage_url = base_url + '/';
 
 casper.test.begin('Verify homepage', 4, function suite(test) {
-  casper.start(public_url, function() {
-    test.assertHttpStatus(200, 'Homepage loaded ok');
+  casper.start(homepage_url, function() {
+    test.assertHttpStatus(200);
     test.assertExists('a[href="' + fakeminder_config.target_site.urls.logoff + '"]', 'Has a link to the logoff page');
     test.assertExists('a[href="' + fakeminder_config.target_site.urls.protected + '"]', 'Has a link to the protected folder');
-    test.assertExists('a[href="/public/logon"]');
+    test.assertExists('a[href="/public/logon"]', 'Has a link to the login page');
   });
 
   casper.run(function() {
     test.done();
   });
+});
+
+casper.test.begin('Verify logoff page', 3, function suite(test) {
+  casper.start(homepage_url);
+
+  casper.then(function() {
+    this.click('a[href="' + fakeminder_config.target_site.urls.logoff + '"]');
+  });
+
+  casper.then(function() {
+    test.assertHttpStatus(200);
+    test.assertTitle('Logoff');
+    test.assertTextExists('Your are now logged off. Have a nice day!');
+  })
+
+  casper.run(function() {
+    test.done();
+  })
+});
+
+casper.test.begin('Verify protected page', 3, function suite(test) {
+  casper.start(homepage_url);
+
+  casper.then(function() {
+    this.click('a[href="' + fakeminder_config.target_site.urls.protected + '"]');
+  });
+
+  casper.then(function() {
+    test.assertHttpStatus(302);
+  })
+
+  casper.run(function() {
+    test.done();
+  })
 });
