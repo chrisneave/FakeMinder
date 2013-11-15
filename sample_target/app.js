76f00d2ecb0d6b16a07ea78ccefe1130e1e94402
Chris Neave <chrispneave@gmail.com>
2013-11-15 21:12:56 +0000

    Finish tests for the protected page
    Remove dependency on the bodyParser middleware.

--- sample_target/app.js
+++ sample_target/app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
--- sample_target/test/app_test.js
+++ sample_target/test/app_test.js
@@ -1,9 +1,12 @@
-var fs = require('fs');
-var json = fs.read('./config.json', 'utf8');
-var fakeminder_config = JSON.parse(json);
-var base_url = fakeminder_config.target_site.root;
-var homepage_url = base_url + '/';
+var fs = require('fs'),
+    json = fs.read('./config.json', 'utf8'),
+    fakeminder_config = JSON.parse(json),
+    base_url = fakeminder_config.target_site.root,
+    homepage_url = base_url + '/';
 
+/*
+ * Homepage
+ */
 casper.test.begin('Verify homepage', 4, function suite(test) {
   casper.start(homepage_url, function() {
     test.assertHttpStatus(200);
@@ -17,6 +20,9 @@ casper.test.begin('Verify homepage', 4, function suite(test) {
   });
 });
 
+/*
+ * Logoff page
+ */
 casper.test.begin('Verify logoff page', 3, function suite(test) {
   casper.start(homepage_url);
 
@@ -28,13 +34,16 @@ casper.test.begin('Verify logoff page', 3, function suite(test) {
     test.assertHttpStatus(200);
     test.assertTitle('Logoff');
     test.assertTextExists('Your are now logged off. Have a nice day!');
-  })
+  });
 
   casper.run(function() {
     test.done();
-  })
+  });
 });
 
+/*
+ * Protected page
+ */
 casper.test.begin('Verify protected page', 3, function suite(test) {
   casper.start(homepage_url);
 
@@ -43,10 +52,12 @@ casper.test.begin('Verify protected page', 3, function suite(test) {
   });
 
   casper.then(function() {
-    test.assertHttpStatus(302);
-  })
+    test.assertHttpStatus(200);
+    test.assertTitle('Not authenticated');
+    test.assertTextExists('Your are not authenticated. Please login before accessing this resource.');
+  });
 
   casper.run(function() {
     test.done();
-  })
+  });
 });
