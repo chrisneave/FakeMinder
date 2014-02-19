cf024c934ed7244a4730a0167d6ee70945768874
Chris Neave <chrispneave@gmail.com>
2014-02-19 22:09:23 +0000

    Support for more advanced URL pattern matching definitions
    Wildcards are now supported.
    Each URL pattern can be marked as protected or not.
    A default protection level can now be configured if a matching pattern is not found for a given URL.
    Also moved the path definitions for redirects to the siteminder section of the configuration. This makes more sense currently as the redirect are a SiteMinder specific configuration.

--- test/fakeminder_test.js
+++ test/fakeminder_test.js
@@ -772,4 +772,130 @@ describe('FakeMinder', function() {
       expect(response.headers['Location']).to.equal(expected);
     });
   });
+
+  describe.only('#_isProtected', function() {
+    var url_config;
+
+    beforeEach(function() {
+      url_config = {};
+      url_config.protected_by_default = true;
+    });
+
+    describe('protected_by_default is true', function() {
+      beforeEach(function() {
+        url_config.protected_by_default = true;
+      });
+
+      it('uses the default value is there are no rules', function() {
+        // Arrange
+        var url = '/abc/123',
+            result;
+
+        // Act
+        result = subject._isProtected(url_config, url);
+
+        // Assert
+        expect(result).to.eql({ url: '{default}', protected: true});
+      });
+    });
+
+    describe('protected_by_default is false', function() {
+      beforeEach(function() {
+        url_config.protected_by_default = false;
+      });
+
+      it('uses the default value is there are no rules', function() {
+        // Arrange
+        var url = '/abc/123',
+            result;
+
+        // Act
+        result = subject._isProtected(url_config, url);
+
+        // Assert
+        expect(result).to.eql({ url: '{default}', protected: false});
+      });
+    });
+
+    it('can match a URL exactly', function() {
+      // Arrange
+      url_config.paths = [{ url: '/abc/123', protected: false }];
+
+      // Act
+      var result = subject._isProtected(url_config, '/abc/123');
+
+      // Assert
+      expect(result).to.eql(url_config.paths[0]);
+    });
+
+    it('can fail to match a URL exactly', function() {
+      // Arrange
+      url_config.paths = [{ url: '/abc/123/xyz', protected: false }];
+
+      // Act
+      var result = subject._isProtected(url_config, '/abc/123');
+
+      // Assert
+      expect(result).to.eql({ url: '{default}', protected: true});
+    });
+
+    it('matches the first path in the order it is configured', function() {
+      // Arrange
+      url_config.paths = [];
+      url_config.paths.push({ url: '/abc/123', protected: false });
+      url_config.paths.push({ url: '/abc/123', protected: true });
+
+      // Act
+      var result = subject._isProtected(url_config, '/abc/123');
+
+      // Assert
+      expect(result).to.eql(url_config.paths[0]);
+    });
+
+    it('handles paths with trailing slashes', function() {
+      // Arrange
+      url_config.paths = [{ url: '/abc/123/', protected: false }];
+
+      // Act
+      var result = subject._isProtected(url_config, '/abc/123');
+
+      // Assert
+      expect(result).to.eql(url_config.paths[0]);
+    });
+
+    describe('wildcard support', function() {
+      it('matches against a leading wildcard', function() {
+        // Arrange
+        url_config.paths = [{ url: '*/xyz', protected: false }];
+
+        // Act
+        var result = subject._isProtected(url_config, '/abc/123/xyz');
+
+        // Assert
+        expect(result).to.eql(url_config.paths[0]);
+      });
+
+      it('matches against a trailing wildcard', function() {
+        // Arrange
+        url_config.paths = [{ url: '/abc/*', protected: false }];
+
+        // Act
+        var result = subject._isProtected(url_config, '/abc/123');
+
+        // Assert
+        expect(result).to.eql(url_config.paths[0]);
+      });
+
+      it('matches against a combined wildcard', function() {
+        // Arrange
+        url_config.paths = [{ url: '*/123/*', protected: false }];
+
+        // Act
+        var result = subject._isProtected(url_config, '/abc/123/xyz');
+
+        // Assert
+        expect(result).to.eql(url_config.paths[0]);
+      });
+    });
+  });
 });
